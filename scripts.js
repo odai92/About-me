ae69e7183da84d81213281bf8a2232c7ed31af4d
Odai Qudah <odai92@live.com>
2020-12-28 22:12:57 +0200

    update

--- scripts.js
+++ scripts.js
@@ -1,62 +1,62 @@
-// 'use stric'
+'use stric'
 
-// userName = prompt('Whats Your Name??');
-// alert(userName + ", Happy to see you on my page!!");
+userName = prompt('Whats Your Name??');
+alert(userName + ", Happy to see you on my page!!");
 
 
-// var firQue = prompt('Are you feeling good today? yes or no.');
-// console.log(firQue);
+var firQue = prompt('Are you feeling good today? yes or no.');
+console.log(firQue);
 
-// if(firQue.toLowerCase() === "yes"){
-//   alert('im happy to hear u r good');
-// } else if(firQue.toLowerCase() === 'no'){
-//   var why= prompt('why u r not feeling good?');
-// } else {
-//   alert('Refresh your page and choose one of the answers!');
-// }
+if(firQue.toLowerCase() === "yes"){
+  alert('im happy to hear u r good');
+} else if(firQue.toLowerCase() === 'no'){
+  var why= prompt('why u r not feeling good?');
+} else {
+  alert('Refresh your page and choose one of the answers!');
+}
 
-// var secQue = prompt('Have you ever been to Qatar? yes or no');
-// console.log(secQue);
+var secQue = prompt('Have you ever been to Qatar? yes or no');
+console.log(secQue);
 
-// if(secQue.toLowerCase() === "yes"){
-//   alert('Its a nice country!');
-// } else if(secQue.toLowerCase() === 'no'){
-//   var why= prompt('you most visit it');
-// } else {
-//   alert('Refresh your page and choose one of the answers!');
-// }
+if(secQue.toLowerCase() === "yes"){
+  alert('Its a nice country!');
+} else if(secQue.toLowerCase() === 'no'){
+  var why= prompt('you most visit it');
+} else {
+  alert('Refresh your page and choose one of the answers!');
+}
 
-// var thiQue = prompt('Did you enjoyed yesterday? yes or no.');
-// console.log(thiQue);
+var thiQue = prompt('Did you enjoyed yesterday? yes or no.');
+console.log(thiQue);
 
-// if(thiQue.toLowerCase() === "yes"){
-//   alert('Hope today as well.');
-// } else if(thiQue.toLowerCase() === 'no'){
-//   var why= prompt('maybe today will be better.');
-// } else {
-//   alert('Refresh your page and choose one of the answers!');
-// }
+if(thiQue.toLowerCase() === "yes"){
+  alert('Hope today as well.');
+} else if(thiQue.toLowerCase() === 'no'){
+  var why= prompt('maybe today will be better.');
+} else {
+  alert('Refresh your page and choose one of the answers!');
+}
 
-// var forQue = prompt('You will watch the match tonight? yes or no.');
-// console.log(forQue);
+var forQue = prompt('You will watch the match tonight? yes or no.');
+console.log(forQue);
 
-// if(forQue.toLowerCase() === "yes"){
-//   alert('Hope your team will win!');
-// } else if(forQue.toLowerCase() === 'no'){
-//   var why= prompt('why u r not feeling good?');
-// } else {
-//   alert('RIts boaring one anyway,,');
-// }
+if(forQue.toLowerCase() === "yes"){
+  alert('Hope your team will win!');
+} else if(forQue.toLowerCase() === 'no'){
+  var why= prompt('why u r not feeling good?');
+} else {
+  alert('RIts boaring one anyway,,');
+}
 
-// var fifQue = prompt('Do like Tennis? yes or no.');
-// console.log(firQue);
+var fifQue = prompt('Do like Tennis? yes or no.');
+console.log(firQue);
 
-// if(fifQue.toLowerCase() === "yes"){
-//   alert('There is an offer at the club x');
-// } else if(fifQue.toLowerCase() === 'no'){
-//   var why= prompt('try to play it once!');
-// } else {
-//   alert('Refresh your page and choose one of the answers!');
-// }
+if(fifQue.toLowerCase() === "yes"){
+  alert('There is an offer at the club x');
+} else if(fifQue.toLowerCase() === 'no'){
+  var why= prompt('try to play it once!');
+} else {
+  alert('Refresh your page and choose one of the answers!');
+}
 
-// alert(userName);
+alert(userName);
